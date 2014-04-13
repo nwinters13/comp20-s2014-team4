0ecd506c3d9dc7da2ff838c8fc20c4262e7ff602
JMRosenberg <Jacob.Rosenberg@Tufts.edu>
2014-04-13 18:41:54 -0400

    added mongo to app

--- web.js
+++ web.js
@@ -1,7 +1,18 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var mongo = require('mongodb');
 var app = express();
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+
+/* Mongo connection example:
+mongo.Db.connect(mongoUri, function (err, db) {
+    db.collection('mydocs', function (er, collection) {
+	collection.insert({'mykey': 'myvalue'}, {safe: true}, function (er, rs) {
+	});
+    });
+});
+*/
 
 app.use(logfmt.requestLogger());
 
