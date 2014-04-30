f5eb608067849c18c3a06a7ca829b73670fa4e1b
nwinters13 <nathan.winters@tufts.edu>
2014-04-30 11:50:48 -0400

    Adding in the mandrill API
    Code was gone :( NOW IT'S BACK

--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,44 @@
-$(function(){
-var textfield = $("input[name=user]");
-            $('button[type="submit"]').click(function(e) {
-                e.preventDefault();
-                //little validation just to check username
-                if (textfield.val() != "") {
-                    //$("body").scrollTo("#output");
-                    $("#output").addClass("alert alert-success animated fadeInUp").html("Welcome back " + "<span style='text-transform:uppercase'>" + textfield.val() + "</span>");
-                    $("#output").removeClass(' alert-danger');
-                    $("input").css({
-                    "height":"0",
-                    "padding":"0",
-                    "margin":"0",
-                    "opacity":"0"
-                    });
-                    //change button text 
-                    $('button[type="submit"]').html("continue")
-                    .removeClass("btn-info")
-                    .addClass("btn-default").click(function(){
-                    $("input").css({
-                    "height":"auto",
-                    "padding":"10px",
-                    "opacity":"1"
-                    }).val("");
-                    });
-                    
-                    //show avatar
-                    $(".avatar").css({
-                        "background-image": "url('http://api.randomuser.me/0.3.2/portraits/women/35.jpg')"
-                    });
-                } else {
-                    //remove success mesage replaced with error message
-                    $("#output").removeClass(' alert alert-success');
-                    $("#output").addClass("alert alert-danger animated fadeInUp").html("sorry enter a username ");
-                }
-                //console.log(textfield.val());
+function onLogin(){
 
-            });
-});
+var newUser = true
+var msg = document.getElementById("email").value;
+if(msg == localStorage['CEemail']){
+    newUser = false;
+}
+else {
+    localStorage['CEemail'] = msg;
+}
+
+// create a new instance of the Mandrill class with your API key
+var m = new mandrill.Mandrill('TnrrHAw7RE87k-8ejmpQ0A');
+
+// create a variable for the API call parameters
+var params = {
+    "message": {
+        "from_email":"CostEaterTeam@gmail.com",
+        "to":[{"email": msg}],
+        "subject": "Thanks from the Cost Eater Team",
+        "text": "We here at the Cost Eater Headquarters would like to thank you for taking the time to enjoy what Cost Eater has to offer!"
+    }
+};
+
+if(newUser == true){
+    sendTheMail(m, params);
+}
+
+}
+
+
+
+
+
+
+function sendTheMail(m, params) {
+// Send the email!
+
+    m.messages.send(params, function(res) {
+        console.log(res);
+    }, function(err) {
+        console.log(err);
+    });
+}
