4f93d2fd970426b6c360f500a4feb1a20e9ec26b
JMRosenberg <Jacob.Rosenberg@Tufts.edu>
2014-04-30 05:05:46 -0400

    another CORS attempt

--- web.js
+++ web.js
@@ -13,6 +13,7 @@ app.use(bodyParser());
 // TEMPORARY  ===  REMOVE BEFORE FINAL =================
 
 app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", null);
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "*");//X-Requested-With");
