c83e91d35d856296d612bf4c4316d86a29389b3d
nwinters13 <nathan.winters@tufts.edu>
2014-04-30 13:04:42 -0400

    adding in more to login

--- public/js/login.js
+++ public/js/login.js
@@ -1,35 +1,32 @@
 function onLogin(){
 
-var newUser = true
-var msg = document.getElementById("email").value;
-if(msg == localStorage['CEemail']){
-    newUser = false;
-}
-else {
-    localStorage['CEemail'] = msg;
-}
-
-// create a new instance of the Mandrill class with your API key
-var m = new mandrill.Mandrill('TnrrHAw7RE87k-8ejmpQ0A');
-
-// create a variable for the API call parameters
-var params = {
-    "message": {
-        "from_email":"CostEaterTeam@gmail.com",
-        "to":[{"email": msg}],
-        "subject": "Thanks from the Cost Eater Team",
-        "text": "We here at the Cost Eater Headquarters would like to thank you for taking the time to enjoy what Cost Eater has to offer!"
+    var newUser = true
+    var msg = document.getElementById("email").value;
+    if(msg == localStorage['CEemail']){
+        newUser = false;
     }
-};
+    else {
+        localStorage['CEemail'] = msg;
+    }
 
-if(newUser == true){
-    sendTheMail(m, params);
+    // create a new instance of the Mandrill class with your API key
+    var m = new mandrill.Mandrill('TnrrHAw7RE87k-8ejmpQ0A');
+
+    // create a variable for the API call parameters
+    var params = {
+        "message": {
+            "from_email":"CostEaterTeam",
+            "to":[{"email": msg}],
+            "subject": "Thanks from the Cost Eater Team",
+            "text": "We here at the Cost Eater Headquarters would like to thank you for taking the time to enjoy what Cost Eater has to offer!"
+        }
+    };
+
+    if(newUser == true){
+        sendTheMail(m, params);
+    }
+
 }
-
-}
-
-
-
 
 
 
