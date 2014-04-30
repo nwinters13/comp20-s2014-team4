6e9ea889e9a5f7fa8450c4412b58e6be6cffc0e6
JMRosenberg <Jacob.Rosenberg@Tufts.edu>
2014-04-29 22:40:50 -0400

    post.json all set

--- web.js
+++ web.js
@@ -2,12 +2,13 @@
 var express = require("express");
 var mongo = require('mongodb');
 var connect = require('connect');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
-
+app.use(bodyParser());
 
 /* Mongo connection example:
 mongo.Db.connect(mongoUri, function (err, db) {
@@ -23,8 +24,7 @@ app.get('/', function(req, res) {
 });
 
 app.post('/post.json', function(req, res){
-    console.log(req);
-    /*if(req.body.email === undefined || req.body.email === null){
+    if(req.body.email === undefined || req.body.email === null){
 	res.send(400);
     }
     else if(req.body.trip === undefined || req.body.email === null){
@@ -33,18 +33,23 @@ app.post('/post.json', function(req, res){
     else{
 	mongo.Db.connect(mongoUri, function (err, db) {
 	    db.collection('mydocs', function (er, collection) {
-
 		var userEmail = req.body.email;
 		var newTrip = req.body.trip;
-
-		var user = collection.find({email: userEmail});
-		
-		console.log(user);
-		
-		res.send();
+		var user = collection.find({email: userEmail}).toArray(function(errr, x){
+		    if(x.length == 0){
+			collection.insert({"email": userEmail, "trips": [newTrip]}, function (err, r){});
+			res.send();
+		    }
+		    else{
+			newX = x[0];
+			newX.trips[x[0].trips.length] = newTrip;
+			collection.remove({"email": userEmail}, 1, function(){});
+			collection.insert(newX, function(errrr, rr){});
+		    }
+		});
 	    });
 	});
-    }*/
+    }
     res.send();
 });
 
