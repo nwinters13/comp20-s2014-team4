f38569c4292b0199a2dded4af0a388905cee0385
JMRosenberg <Jacob.Rosenberg@Tufts.edu>
2014-04-29 21:31:30 -0400

    express version, and static files

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 // web.js
 var express = require("express");
-//var logfmt = require("logfmt");
 var mongo = require('mongodb');
+var connect = require('connect');
+
 var app = express();
+
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+
+
 
 /* Mongo connection example:
 mongo.Db.connect(mongoUri, function (err, db) {
@@ -14,13 +18,38 @@ mongo.Db.connect(mongoUri, function (err, db) {
 });
 */
 
-
-//app.use(logfmt.requestLogger());
-
-//app.use("/public", express.static(__dirname + '/public'));
-
 app.get('/', function(req, res) {
-  res.sendfile('./public/login.html');
+    res.sendfile('./public/login.html');
+});
+
+app.post('/post.json', function(req, res){
+    console.log(req);
+    /*if(req.body.email === undefined || req.body.email === null){
+	res.send(400);
+    }
+    else if(req.body.trip === undefined || req.body.email === null){
+	res.send(400);
+    }
+    else{
+	mongo.Db.connect(mongoUri, function (err, db) {
+	    db.collection('mydocs', function (er, collection) {
+
+		var userEmail = req.body.email;
+		var newTrip = req.body.trip;
+
+		var user = collection.find({email: userEmail});
+		
+		console.log(user);
+		
+		res.send();
+	    });
+	});
+    }*/
+    res.send();
+});
+
+app.get('/bootstrap.min.css', function(req, res){
+    res.sendfile('./public/css/bootstrap.min.css');
 });
 
 var port = Number(process.env.PORT || 5000);
